0754601021a96094660468dfff3b4f5e4fffc160
mojim <mojima16@gmail.com>
2019-02-20 17:42:27 +0100

    Setters & Getters Address (Public and Privates keys)

--- ExamenLorenzoMoreno/src/main/java/org/lasencinas/examenlorenzomoreno/Address.java
+++ ExamenLorenzoMoreno/src/main/java/org/lasencinas/examenlorenzomoreno/Address.java
@@ -1,7 +1,42 @@
 
 package org.lasencinas.examenlorenzomoreno;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import org.lasencinas.examenlorenzomoreno.GenSig;
 
 public class Address {
+    
+    //ATRIBUTOS
+    private PublicKey PK = null;
+    private PrivateKey SK = null;
+    private double balance = 0;
+    private String EZI = null;
+    
+    
+    public Address() {};
+    
+    //Setters & Getters
+    
+    public void SetPK(PublicKey PK) {
+        this.PK = PK;
+    }
+    
+    public PublicKey getPK() {
+        return this.PK;
+    }
+    
+    public void SetSK(PrivateKey SK) {
+        this.SK = SK;
+    }
+    
+    public PrivateKey getSK() {
+        return this.SK;
+    }
+    
+    //MÉTODOS
+    
+    
     
 }
